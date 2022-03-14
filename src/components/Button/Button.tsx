27406d8b4cbaeeb5bccd6926b710a45fb4a7e246
Christian Lara <christianramirezlara23@gmail.com>
2022-03-14 10:02:33 -0600

    Work in Button component.

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import './Buttons.scss';
+
+interface ButtonProps {
+  buttonTextContent: string;
+}
+
+const Button = ({ buttonTextContent }: ButtonProps) => {
+  return <button className="button">{buttonTextContent}</button>;
+};
+
+export default Button;
