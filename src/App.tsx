6074c4825a0f845aa1de91d4d10d58801e8f3bf8
Christian Lara <christianramirezlara23@gmail.com>
2022-03-14 09:33:20 -0600

    Work on Hero layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import HeaderFooter from './layouts/HeaderFooter/HeaderFooter';
+import Hero from './layouts/Hero/Hero';
 
 function App() {
   return (
     <div className="App">
       <HeaderFooter type="header" />
+      <Hero />
     </div>
   );
 }
---
+++ src/layouts/Hero/Hero.tsx
@@ -0,0 +1,15 @@
+import './Hero.scss';
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero__content">
+        <div className="hero__content__texts">
+          <h1>WHERE FRIENDSHIP BEGINS</h1>
+          <p>Discover facts and images about man's best friend, the doggo</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
